3576b3fbf117b6508697b637e1085da5f8b8e7f4
Jordan Wathen <jordan.wathen5@gmail.com>
2021-03-29 16:22:52 +0100

    Console now takes the typed user input, not just the dummy input which was hard coded

--- frontEndFromScratch/my-app/src/components/CodeConsole.js
+++ frontEndFromScratch/my-app/src/components/CodeConsole.js
@@ -1,25 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function CodeConsole(props) {
-  return (
-    <div className = "console-content">
-      {props.consoleStrings.map((str, idx) => {
-        return (
-          <div className = {str.type} key={idx}>
-            {str.text}
-          </div>  
+export default class CodeConsole extends Component{
+
+  constructor(props) {
+    super(props);
+    this.onChangeInput = this.onChangeInput.bind(this);
+ 
+    this.state = {
+        input: '',
+    }
+  }
+  
+  onChangeInput(e) {
+      this.setState({
+        input: e.target.value + "\n"
+      })
+  }  
+
+  render(){
+    return (
+      <div className = "console-content">
+        {this.props.consoleStrings.map((str, idx) => {
+          return (
+            <div className = {str.type} key={idx}>
+              {str.text}
+            </div>  
+          )
+        })}
+        <input
+                className="console-input"
+                onKeyDown={ event => {if (event.key === 'Enter') {
+                  event.preventDefault()
+                  this.props.sendExecutionInput(this.state.input)}}}
+                type='text'
+                autoComplete='off'
+                onChange = {this.onChangeInput}
+              />
+      </div>
         )
-      })}
-      <input
-              className="console-input"
-              onKeyDown={event => {if (event.key === 'Enter') props.sendExecutionInput("Helloooo")}}
-              type='text'
-              autoComplete='off'
-            />
-    </div>
-  );
+    }
 }
-
-export default CodeConsole;
-
-
