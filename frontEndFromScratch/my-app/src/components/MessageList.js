aaad803508e95d15f2a6fd1e3cf62fdaca33d80f
Jordan Wathen <jordan.wathen5@gmail.com>
2021-04-10 18:29:17 +0100

    Initial connection to audio visual established

--- frontEndFromScratch/my-app/src/components/MessageList.js
+++ frontEndFromScratch/my-app/src/components/MessageList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import styles from '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
-import { ConversationHeader, VoiceCallButton, VideoCallButton, InfoButton, ChatContainer, MessageList as ChatScopeMessageList, Message, MessageInput, Avatar } from '@chatscope/chat-ui-kit-react';
+import { ConversationHeader, VoiceCallButton, VideoCallButton, InfoButton, ChatContainer, MessageList as ChatScopeMessageList, Message, MessageInput, Avatar, Button, Loader } from '@chatscope/chat-ui-kit-react';
     
 
 
@@ -16,6 +16,16 @@ export class MessageList extends Component {
   }
     
   render() {
+    var avComponent = <div/>
+    if (this.state.voiceState == "initiate")
+    {
+      avComponent = <div><Loader>Starting voice call</Loader><Button border onClick={() => this.setState({voiceState:"none"})}>Cancel</Button></div>
+    }
+    else if (this.state.videoState == "initiate")
+    {
+      avComponent = <div><Loader>Starting video call</Loader><Button border onClick={() => this.setState({videoState:"none"})}>Cancel</Button></div>
+    }
+
     return (
     <>
       <div style={{ position:"relative", height: "500px" }}>
@@ -25,13 +35,17 @@ export class MessageList extends Component {
                 <Avatar src={`/images/${this.props.otherUserId}.ico`} name={this.props.otherUserName} />
                 <ConversationHeader.Content userName={this.props.otherUserName} info="Active 10 mins ago" />                                   
                 <ConversationHeader.Actions>                                                                             
-                  <VoiceCallButton title="Start voice call" onClick={() => this.setState({voiceState:"initiate"})} />
-                  <VideoCallButton title="Start video call" onClick={() => this.setState({videoState:"initiate"})}/>
+                  <VoiceCallButton title="Start voice call" disabled={this.state.voiceState !== "none" || this.state.videoState !== "none"} onClick={() => this.initiateVoiceCall()} />
+                  <VideoCallButton title="Start video call" disabled={this.state.voiceState !== "none" || this.state.videoState !== "none"} onClick={() => this.initiateVideoCall()}/>
                   <InfoButton title="Eventually shows active help request?"/>
                 </ConversationHeader.Actions>
             </ConversationHeader>
 
+
             <ChatScopeMessageList>
+            <ChatScopeMessageList.Content>
+            {avComponent}
+
               {this.props.chatMessages.map(message => {                  
                   if(message.from === this.props.userId){
                     return (
@@ -53,6 +67,7 @@ export class MessageList extends Component {
                 
                 })
               }
+              </ChatScopeMessageList.Content>
             </ChatScopeMessageList>
             <MessageInput 
               attachButton={false} 
@@ -63,8 +78,25 @@ export class MessageList extends Component {
     </>  
     )
   }
-}
 
+  initiateVoiceCall() {
+    this.setState({voiceState:"initiate"});
+    const stream = window.navigator.mediaDevices.getUserMedia(
+      {
+        audio: true,
+      },
+    );
+  }
+
+  initiateVideoCall() {
+    this.setState({videoState:"initiate"});
+    const stream = window.navigator.mediaDevices.getUserMedia(
+      {
+        video: true,
+        audio: true,
+      },
+    );
+  }}
 
 export default MessageList
 
